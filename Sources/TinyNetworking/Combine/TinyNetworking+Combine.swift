0e46bdafcb7706b4bc064c43772d9d7c94baf93c
Joan Disho <dishojoan@gmail.com>
2019-09-04 22:49:56 +0200

    create the Combine version of TinyNetworking

---
+++ Sources/TinyNetworking/Combine/TinyNetworking+Combine.swift
@@ -0,0 +1,39 @@
+//
+//  TinyNetworking+Combine.swift
+//  TinyNetworking
+//
+//  Created by Joan Disho on 04.09.19.
+//  Copyright © 2019 Joan Disho. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+public extension TinyNetworking {
+
+    func requestPublisher(
+        resource: R,
+        session: TinyNetworkingSession = URLSession.shared,
+        queue: DispatchQueue = .main
+        ) -> AnyPublisher<Response, Error> {
+        let publisher = TinyNetworkingPublisher<Response> { [weak self] subscriber in
+            return self?.request(
+                resource: resource,
+                session: session,
+                queue: queue,
+                completion: { result in
+                    switch result {
+                    case let .success(response):
+                        _ = subscriber.receive(response)
+                        subscriber.receive(completion: .finished)
+                    case let .failure(error):
+                        subscriber.receive(completion: .failure(error))
+                    }
+                }
+            )
+        }
+        return publisher.eraseToAnyPublisher()
+    }
+}
+
