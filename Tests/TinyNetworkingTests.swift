556e77d20c1337f71db01df1e4e3121c765ae8b5
Alan Steiman <asteiman@gmail.com>
2020-04-07 16:48:36 +0800

    Add tests for TinyNetworking

--- Tests/TinyNetworkingTests.swift
+++ Tests/TinyNetworkingTests.swift
@@ -1,9 +1,9 @@
 //
 //  TinyNetworkingTests.swift
-//  TinyNetworkingTests
+//  TinyNetworking
 //
-//  Created by Joan Disho on 04.03.18.
-//  Copyright © 2018 Joan Disho. All rights reserved.
+//  Created by Alan Steiman on 06/04/2020.
+//  Copyright © 2020 Joan Disho. All rights reserved.
 //
 
 import XCTest
@@ -11,26 +11,108 @@ import XCTest
 
 class TinyNetworkingTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_notNil() {
+        let instance = TinyNetworking<FooResource>()
+        
+        XCTAssertNotNil(instance)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_request_failure_underlyingError() {
+        let instance = TinyNetworking<FooResource>()
+        let mockSession = MockSession(input: MockSession.Input(data: nil, httpURLResponse: nil, error: FooError.random))
+        instance.request(resource: .someEndpoint, session: mockSession, queue: .main) { result in
+            switch result {
+            case .success(_):
+                XCTFail("Unexpected success response")
+            case let .failure(error):
+                if case let TinyNetworkingError.underlying(underlyingError, _) = error {
+                    XCTAssertEqual(underlyingError as? FooError, FooError.random)
+                } else {
+                    XCTFail("Error type does not match")
+                }
+            }
         }
     }
     
+    func test_request_failure_httpError() {
+        let instance = TinyNetworking<FooResource>()
+        let mockSession = MockSession(input: MockSession.Input(data: nil, httpURLResponse: HTTPURLResponse(url: URL(string: "https://www.google.com")!, statusCode: 500, httpVersion: nil, headerFields: nil), error: nil))
+        instance.request(resource: .someEndpoint, session: mockSession, queue: .main) { result in
+            switch result {
+            case .success(_):
+                XCTFail("Unexpected success response")
+            case let .failure(error):
+                if case let TinyNetworkingError.statusCode(response) = error {
+                    XCTAssertEqual(response.httpURLResponse?.statusCode, 500)
+                } else {
+                    XCTFail("Error type does not match")
+                }
+            }
+        }
+    }
+    
+    func test_request_success() {
+        let returnData = Data(base64Encoded: "response")
+        let instance = TinyNetworking<FooResource>()
+        let mockSession = MockSession(input: MockSession.Input(data: returnData, httpURLResponse: HTTPURLResponse(url: URL(string: "https://www.google.com")!, statusCode: 200, httpVersion: nil, headerFields: nil), error: nil))
+        instance.request(resource: .someEndpoint, session: mockSession, queue: .main) { result in
+            switch result {
+            case let .success(response):
+                XCTAssertEqual(response.data, returnData)
+            case .failure(_):
+                XCTFail("Unexpected error response")
+            }
+        }
+    }
+}
+
+enum FooError: Swift.Error {
+    case random
+}
+
+class MockSession: TinyNetworkingSession {
+    
+    struct Input {
+        let data: Data?
+        let httpURLResponse: HTTPURLResponse?
+        let error: Swift.Error?
+    }
+    
+    private let input: Input
+    
+    init(input: Input) {
+        self.input = input
+    }
+    
+    func loadData(with urlRequest: URLRequest, queue: DispatchQueue, completionHandler: @escaping CompletionHandler) -> URLSessionDataTask {
+        
+        completionHandler(Response(urlRequest: urlRequest, data: input.data, httpURLResponse: input.httpURLResponse), input.error)
+        return URLSessionDataTask()
+    }
+}
+
+enum FooResource {
+    case someEndpoint
+}
+
+extension FooResource: Resource {
+    var baseURL: URL {
+       return URL(string: "https://api.unsplash.com")!
+     }
+     
+     var endpoint: Endpoint {
+       switch self {
+       case .someEndpoint:
+         return .get(path: "/someEndpoint")
+       }
+     }
+    
+     var task: Task {
+       var params: [String: Any] = [:]
+       return .requestWithParameters(params, encoding: URLEncoding())
+     }
+     
+     var headers: [String: String] {
+       return ["Authorization": "Bearer xxx"]
+     }
 }
